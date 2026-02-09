e8e7fc1714dbd40fae2d86b350275b956a9d7341
to0ken <evgeniamafinova@gmail.com>
2026-02-09 12:21:52 +0300

    Update admin.py

--- django_13/products/admin.py
+++ django_13/products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Discount
+
 
 @admin.register(Category)
 class CatedoryAdmin(admin.ModelAdmin):
@@ -12,6 +13,21 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter =['available', 'created', 'category' ]
     list_editable = ['price','available']
     prepopulated_fields = {'slug':('name',)}
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ['name', 'get_type', 'value', 'apply_to', 'product', 'active']
+    list_editable = ['active']
+    list_filter = ['active', 'discount_type']
+
+    def get_type(self, obj):
+        return '%' if obj.discount_type == 'percentage' else '₽'
+
+    get_type.short_description = 'Тип'
+
+
+
 
 
 
